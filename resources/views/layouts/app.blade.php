ec4312af1a15fe1e19c0629947289a44aa1bd06d
Maik Pütz <maikpuetz@gmail.com>
2017-03-15 22:51:28 +0100

    Navigation bar was fixed at top so the panels had no margin at top between navigation bar and themselves.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default">
             <div class="container">
                 <div class="navbar-header">
 
