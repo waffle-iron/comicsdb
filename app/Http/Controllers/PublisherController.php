7d6d98b87575fe7aab1bc14550d64cd3a8502874
Maik Pütz <maikpuetz@gmail.com>
2017-04-15 08:01:49 +0200

    Removed unnecessary use

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePublisherRequest;
-use App\Interfaces\DisplayableInterface;
 use App\Repositories\PublisherRepository;
 use Illuminate\Http\Request;
 
