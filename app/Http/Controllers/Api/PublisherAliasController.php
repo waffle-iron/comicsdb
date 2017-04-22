75830d2395f60a6fd0d18f2cf30feeef987c1e79
Maik Pütz <maikpuetz@gmail.com>
2017-04-22 14:45:38 +0200

    Code enhancements

--- app/Http/Controllers/Api/PublisherAliasController.php
+++ app/Http/Controllers/Api/PublisherAliasController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\PublisherAlias;
 use App\Repositories\PublisherAliasRepository;
 use Illuminate\Http\Request;
 
