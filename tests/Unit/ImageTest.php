b58978001eea55ce74822f40538a8a27537a4ffa
Maik Pütz <maikpuetz@gmail.com>
2017-05-19 21:41:05 +0200

    Use database traits

--- tests/Unit/ImageTest.php
+++ tests/Unit/ImageTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class ImageTest extends TestCase
 {
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+    
     public function test_remove_unused_image_files()
     {
         Artisan::call('images:clear');
