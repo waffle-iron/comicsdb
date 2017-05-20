53f41672563e376b91ce9f38e239280fd5c89c8b
Maik Pütz <maikpuetz@gmail.com>
2017-05-20 09:10:19 +0200

    Test request objects

---
+++ tests/Unit/RequestTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Requests\CreatorRequest;
+use App\Http\Requests\IssueRequest;
+use App\Http\Requests\PublisherRequest;
+use App\Http\Requests\VolumeRequest;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RequestTest extends TestCase
+{
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+    public function test_check_creator_request()
+    {
+        $creatorRequest = new CreatorRequest();
+
+        $this->assertTrue($creatorRequest->authorize());
+        $this->assertArrayHasKey('lastname', $creatorRequest->rules());
+    }
+
+    public function test_check_issue_request()
+    {
+        $issueRequest = new IssueRequest();
+
+        $this->assertTrue($issueRequest->authorize());
+        $this->assertArrayHasKey('volume_id', $issueRequest->rules());
+    }
+
+    public function test_check_publisher_request()
+    {
+        $publisherRequest = new PublisherRequest();
+
+        $this->assertTrue($publisherRequest->authorize());
+        $this->assertArrayHasKey('name', $publisherRequest->rules());
+    }
+
+    public function test_check_volume_request()
+    {
+        $volumeRequest = new VolumeRequest();
+
+        $this->assertTrue($volumeRequest->authorize());
+        $this->assertArrayHasKey('name', $volumeRequest->rules());
+    }
+}
